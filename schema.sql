f00db8b91fc7b49afdbd4225f9378e1e05fef5fe
FROMERO63 <romerofrank63@gmail.com>
2023-09-29 20:05:59 -0700

    added tables to schema

--- schema.sql
+++ schema.sql
@@ -12,21 +12,29 @@ CREATE TABLE roles (
     role_id INT NOT NULL AUTO_INCREMENT,
     job_title VARCHAR(100) NOT NULL,
     salary INT NOT NULL,
-    dept_id INT NOT NULL
+    /* reference departments */
+    dept_id INT NOT NULL FOREIGN_KEY
 
+);
+
+CREATE TABLE managers (
+    manager_id INT NOT NULL AUTO_INCREMENT,
+    manager_name VARCHAR(100) NOT NULL,
+    /* reference departments */
+    dept_id INT NOT NULL FOREIGN_KEY
 );
 
 CREATE TABLE employees (
     employee_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    job_title VARCHAR(100) NOT NULL,
-    dept_id INT NOT NULL,
-    salaries INT NOT NULL,
-    managers VARCHAR(100) NOT NULL,
+    job_title VARCHAR(100) NOT NULL FOREIGN_KEY,
+    /* reference other tables
+    dept_id INT NOT NULL, departments
+    salary INT NOT NULL, roles
+    managers VARCHAR(100) NOT NULL, managers
+    */
 );
-
-
 
 
 
