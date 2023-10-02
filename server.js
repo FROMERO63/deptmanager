25bb17c014eb5dbf4c9594080ddcec84a37e8020
FROMERO63 <romerofrank63@gmail.com>
2023-10-02 12:57:46 -0700

    added express,mysql,mysql2, and fixed connection

--- server.js
+++ server.js
@@ -30,96 +30,85 @@ quetions = [
   }
 ];
 
-//variables with select queries
-const selectDepts= db.query('SELECT * FROM departments', function(err,results){
+// //variables with select queries
+// function viewAllDepartments () {db.query('SELECT * FROM departments', function(err,results){
+//   console.log(results);
+// });
+// };
+
+// function viewAllRoles () {db.query('SELECT roles.job_title, roles.role_id, departments.name, roles.salary FROM departments JOIN roles ON departments.id=roles.dept_id', function(err,results){
+//   console.log(results);
+// });
+// };
+
+// function viewAllEmployees () {db.query('SELECT employees.employee_id, employees.first_name, employees.last_name, roles.job_title, departments.name, roles.salary, employees.manager FROM departments JOIN roles ON departments.id=roles.dept_id JOIN employees ON roles.role_id=employees.role_id', function(err,results){
+//   console.log(results);
+// });
+// };
+
+// //add queries
+
+// function addADepartment () {db.query(`INSERT INTO departments (name) VALUES ("${responses["deptname"]}")`, function(err,results){
+//   console.log(results);
+// });
+// };
+
+// function addARole () {db.query(`INSERT INTO roles (job_title, salary, dept_id) VALUES ("${responses["jobtitle"]}",${responses["salary"]},${responses["deptid"]})`, function(err,results){
+//   console.log(results);
+// });
+// };
+// function addAnEmployee () {db.query(`INSERT INTO employees (first_name, last_name, role_id, manager) VALUES ("${responses["firstname"]}","${responses["lastname"]}",${responses["roleid"]},"${responses["manager"]}")`, function(err,results){
+//   console.log(results);
+// });
+// };
+
+// //edit query
+// function editAnEmployee () {db.query(`UPDATE employees SET role_id="${responses["updateroleid"]}" WHERE employee_id="${responses["selectemployeeid"]}"`, function(err,results){
+//   console.log(results);
+// });
+// };
+
+// //select queries for variables
+// //let employees='';
+
+// function getEmployee () {db.query(`SELECT first_name FROM employees`, function(err,results){
+//   let employees=results;
+//   console.log(employees);
+// });
+// };
+
+// function startPrompt(){
+//   inquirer.prompt(questions.list).then((responses) => {
+//   let listResponse = responses["list"];
+//   console.log(listResponse);
+//   if( listResponse === "View All Employees"){
+//     viewAllEmployees;
+//   }
+//   else if( listResponse === "Edit An Employee"){
+//     editAnEmployee;
+//   }
+//   else if( listResponse === "View All Roles"){
+//     viewAllRoles;
+//   }
+//   else if( listResponse === "Add Role"){
+//     addARole;
+//   }
+//   else if( listResponse === "View All Departments"){
+//     viewAllDepartments;
+//   }
+//   else if( listResponse === "Add Department"){
+//     addADepartment;
+//   }
+//   else if( listResponse === "Add Employee"){
+//     addAnEmployee;
+//   }
+//   else (console.log('error in choice'));
+//   }
+// )};
+
+db.query('SELECT * FROM employees', function (err, results) {
   console.log(results);
 });
-
-const selectRoles= db.query('SELECT roles.job_title, roles.role_id, departments.name, roles.salary FROM departments JOIN roles ON departments.id=roles.dept_id', function(err,results){
-  console.log(results);
-});
-
-const selectEmployees= db.query(
-  'SELECT employees.employee_id, employees.first_name, employees.last_name, roles.job_title, departments.name, roles.salary, employees.manager FROM departments JOIN roles ON departments.id=roles.dept_id JOIN employees ON roles.role_id=employees.role_id', function(err,results){
-  console.log(results);
-});
-
-//add queries
-
-const addDept = db.query(`INSERT INTO departments (name) VALUES ("${responses["deptname"]}")`, function(err,results){
-  console.log(results);
-});
-
-const addARole = db.query(`INSERT INTO roles (job_title, salary, dept_id) VALUES ("${responses["jobtitle"]}",${responses["salary"]},${responses["deptid"]})`, function(err,results){
-  console.log(results);
-});
-
-const addAnEmployee = db.query(`INSERT INTO employees (first_name, last_name, role_id, manager) VALUES ("${responses["firstname"]}","${responses["lastname"]}",${responses["roleid"]},"${responses["manager"]}")`, function(err,results){
-  console.log(results);
-});
-
-//edit query
-const editAnEmployee = db.query(`UPDATE employees SET role_id="${responses["updateroleid"]}" WHERE employee_id="${responses["selectemployeeid"]}"`, function(err,results){
-  console.log(results);
-});
-
-//varaibles to functions
-function viewAllEmployees(){
-  selectEmployees;
-};
-
-function updateEmployeeRole(){
-  editAnEmployee;
-};
-
-function viewAllRoles(){
-  selectRoles;
-};
-
-function addRole(){
-  addARole;
-};
-
-function viewAllDepartments(){
-  selectDepts;
-};
-
-function addDepartment(){
-  addDept;
-};
-
-function addEmployee(){
-  addAnEmployee;
-};
-
-function startPrompt(){
-  inquirer.prompt(questions.list).then((responses) => {
-  let listResponse = responses["list"];
-  console.log(listResponse);
-  if( listResponse === "View All Employees"){
-    viewAllEmployees;
-  }
-  else if( listResponse === "Update Employee Role"){
-    updateEmployeeRole;
-  }
-  else if( listResponse === "View All Roles"){
-    viewAllRoles;
-  }
-  else if( listResponse === "Add Role"){
-    addRole;
-  }
-  else if( listResponse === "View All Departments"){
-    viewAllDepartments;
-  }
-  else if( listResponse === "Add Department"){
-    addDepartment;
-  }
-  else if( listResponse === "Add Employee"){
-    addEmployee;
-  }
-  else (console.log('error in choice'));
-  }
-)};
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -129,3 +118,6 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// startPrompt();
+// getEmployee();
